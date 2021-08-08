c805e30237f6126b0094c2d2b3438c3ac3d0531c
allisonjrhee <77465589+allisonjrhee@users.noreply.github.com>
2021-08-08 21:36:51 +0900

    Add files via upload

---
+++ src/specification.cxx
@@ -0,0 +1,13 @@
+#include "specification.hxx"
+
+
+Spec::Spec()
+        : doodle_velocity {0,-5400},
+          doodle_accel {100},
+          block_dims {100,20},
+          block_spacing {100},
+          background_dims {500,800},
+          doodle_dims {10,10},
+          monster_dims {32,34},
+          starting_block {200,700}
+{}
--- src/view.cxx
+++ src/view.cxx
@@ -10,7 +10,7 @@ Color Black{ge211::Color(0,0,0)};
 View::View(Model const& model)
         : model_(model),
           doodle_sprite(ge211::Image_sprite("doodle2.png")),
-          // doodle_sprite({10,10},doodle_color),
+        // doodle_sprite({10,10},doodle_color),
           background_sprite(ge211::Image_sprite("background.png")),
           platform_sprite(ge211::Image_sprite("platform1.png")),
           monster_sprite(ge211::Image_sprite("big_blue_monster.png")),
@@ -33,17 +33,17 @@ View::draw(ge211::Sprite_set& set)
     set.add_sprite(point_sprite, {model_.spec.block_spacing * 4,10}, 2);
     if (model_.game_over) {
         set.add_sprite(doodle_sprite, {model_.doodle.center.x-model_
-        .spec.doodle_adj,
+                .spec.doodle_adj,
                                        model_.spec.background_dims.height}, 2);
         set.add_sprite(gameover_sprite,{125,model_.spec.block_spacing * 4},2);
     } else {
         Position dood {model_.doodle.center.x - model_.spec.doodle_adj, model_
-        .doodle.center.y - model_.spec.icon_center_x};
+                                                                                .doodle.center.y - model_.spec.icon_center_x};
         set.add_sprite(doodle_sprite, dood, 2);
     }
     for (int i = 0; i < model_.blocks.size(); i++) {
         set.add_sprite(platform_sprite,{model_.blocks[i].x, model_.blocks[i]
-        .bottom_left().y},1);
+                .bottom_left().y},1);
     }
     for (int i = 0; i < model_.monsters.size(); i++) {
         set.add_sprite(monster_sprite,{model_.monsters[i].x, model_.monsters[i]
