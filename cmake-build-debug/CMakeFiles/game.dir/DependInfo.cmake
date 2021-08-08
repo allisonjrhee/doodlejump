f56e8776e1a69061a25c87a7e6d8f4b2093fb7ea
allisonjrhee <77465589+allisonjrhee@users.noreply.github.com>
2021-08-08 21:39:22 +0900

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/game.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/allisonrhee/Downloads/DOODLE JUMP COPY 2/src/controller.cxx" "/Users/allisonrhee/Downloads/DOODLE JUMP COPY 2/cmake-build-debug/CMakeFiles/game.dir/src/controller.cxx.o"
+  "/Users/allisonrhee/Downloads/DOODLE JUMP COPY 2/src/doodle.cxx" "/Users/allisonrhee/Downloads/DOODLE JUMP COPY 2/cmake-build-debug/CMakeFiles/game.dir/src/doodle.cxx.o"
+  "/Users/allisonrhee/Downloads/DOODLE JUMP COPY 2/src/main.cxx" "/Users/allisonrhee/Downloads/DOODLE JUMP COPY 2/cmake-build-debug/CMakeFiles/game.dir/src/main.cxx.o"
+  "/Users/allisonrhee/Downloads/DOODLE JUMP COPY 2/src/model.cxx" "/Users/allisonrhee/Downloads/DOODLE JUMP COPY 2/cmake-build-debug/CMakeFiles/game.dir/src/model.cxx.o"
+  "/Users/allisonrhee/Downloads/DOODLE JUMP COPY 2/src/specification.cxx" "/Users/allisonrhee/Downloads/DOODLE JUMP COPY 2/cmake-build-debug/CMakeFiles/game.dir/src/specification.cxx.o"
+  "/Users/allisonrhee/Downloads/DOODLE JUMP COPY 2/src/view.cxx" "/Users/allisonrhee/Downloads/DOODLE JUMP COPY 2/cmake-build-debug/CMakeFiles/game.dir/src/view.cxx.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "../.cs211/lib/ge211/src/../include"
+  ".cs211/lib/ge211/src/../include"
+  "/usr/local/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/allisonrhee/Downloads/DOODLE JUMP COPY 2/cmake-build-debug/.cs211/lib/ge211/src/CMakeFiles/ge211.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /Users/allisonrhee/Downloads/DOODLE JUMP COPY 2
+# Build directory: /Users/allisonrhee/Downloads/DOODLE JUMP COPY 2/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(Test_model_test "model_test")
+set_tests_properties(Test_model_test PROPERTIES  _BACKTRACE_TRIPLES "/Users/allisonrhee/Downloads/DOODLE JUMP COPY 2/.cs211/cmake/211commands.cmake;90;add_test;/Users/allisonrhee/Downloads/DOODLE JUMP COPY 2/CMakeLists.txt;21;add_test_program;/Users/allisonrhee/Downloads/DOODLE JUMP COPY 2/CMakeLists.txt;0;")
+subdirs(".cs211/lib/catch")
+subdirs(".cs211/lib/ge211")
